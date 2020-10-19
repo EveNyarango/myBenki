671d66f5a94e3a85377eb973e21a6d8658e620fd
EveNyarango <evenyarango@gmail.com>
2020-10-20 00:33:48 +0300

    add if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,50 @@
-let add = function(salary, wages, business, bonus, others){
+// income 
+let add1 = function (salary, wages, business, bonus, others) {
     return salary + wages + business + bonus + others
 }
 
-
 $(document).ready(function () {
+
     $('#income').submit(function (event) {
         event.preventDefault();
         let nameInput = $('#name').val();
+       
         let salary = parseInt($("#salary").val());
         let wages = parseInt($("#wages").val());
-        let business = parseInt($("bizna").val());
+        let business = parseInt($("#bizna").val());
         let bonus = parseInt($("#bonus").val());
-        let others = parseInt($("#others").val());
-        let result = add(salary, wages, business, bonus, others);
+        let others = parseInt($("#others1").val());
+        let result = add1(salary, wages, business, bonus, others);
         alert(result);
+        
     })
 
 })
+
+// Expenditure
+let add = function (rent, food, medical, fees, entertainment, others2) {
+    return rent + food + medical + fees + entertainment + others2
+}
+
+$(document).ready(function () {
+    $('#expenditure').submit(function (event) {
+        event.preventDefault();
+        let rent = parseInt($("#rent").val());
+        let food = parseInt($("#food").val());
+        let medical = parseInt($("#medical").val());
+        let fees = parseInt($("#fees").val());
+        let entertainment = parseInt($("#entertain").val());
+        let others2 = parseInt($("#others2").val());
+        // alert(others2)
+        let result2 = add(rent, food, medical, fees, entertainment, others2);
+        alert(result2)
+    })
+})
+
+// alerts expense 
+if(add1 >= add){
+    // event.preventDefault();
+alert("Thank you")
+}else{
+    alert("sorry")
+}
